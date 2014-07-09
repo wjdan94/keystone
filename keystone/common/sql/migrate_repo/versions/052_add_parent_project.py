06ddf7c281df17c1205fddf0359cb33ef51c30cc
Rodrigo Duarte Sousa <rodrigods@lsd.ufcg.edu.br>
2014-07-09 16:32:25 -0300

    Fix sql upgrade tests

--- keystone/common/sql/migrate_repo/versions/052_add_parent_project.py
+++ keystone/common/sql/migrate_repo/versions/052_add_parent_project.py
@@ -46,12 +46,12 @@ def downgrade(migrate_engine):
     meta = sql.MetaData()
     meta.bind = migrate_engine
 
+    project_table = sql.Table('project', meta, autoload=True)
+    project_table.drop_column('parent_project_id')
+
     # SQLite does not support constraints, and querying the constraints
     # raises an exception
     if migrate_engine.name == 'sqlite':
         return
 
     migration_helpers.remove_constraints(list_constraints(migrate_engine))
-
-    project_table = sql.Table('project', meta, autoload=True)
-    project_table.drop_column('parent_project_id')
