3349143099e018bb34fa7f65e341de965d072a4e
Rodrigo Duarte <rduartes@redhat.com>
2016-06-01 19:14:16 -0300

    Small fix

--- keystone_tempest_plugin/tests/scenario/test_saml2_federated_authentication.py
+++ keystone_tempest_plugin/tests/scenario/test_saml2_federated_authentication.py
@@ -79,7 +79,7 @@ class TestSaml2FederatedAuthentication(base.BaseIdentityTest):
 
         relay_state = saml2_authn_request.xpath(
             self.ECP_RELAY_STATE, namespaces=self.ECP_SAML2_NAMESPACES)[0]
-        resp, body = (
+        resp = (
             self.saml2_client.send_service_provider_saml2_authn_response(
                 saml2_idp_authn_response, relay_state, idp_consumer_url))
 
