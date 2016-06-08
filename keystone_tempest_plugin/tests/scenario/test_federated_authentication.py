ab066df7419c52f5325c0519d75af049539fc8e7
Rodrigo Duarte <rduartes@redhat.com>
2016-06-08 16:09:24 -0300

    Fix name

--- keystone_tempest_plugin/tests/scenario/test_federated_authentication.py
+++ keystone_tempest_plugin/tests/scenario/test_federated_authentication.py
@@ -117,7 +117,8 @@ class TestSaml2EcpFederatedAuthentication(base.BaseIdentityTest):
         self.assertNotEmpty(projects)
 
         # Get a scoped token to one of the listed projects
-        body = self.token.auth(project_id=projects[0]['id'], token=token_id)
+        body = self.tokens_client.auth(
+            project_id=projects[0]['id'], token=token_id)
         print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
         print(body)
         print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
